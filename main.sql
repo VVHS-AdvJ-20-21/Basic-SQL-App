e0f9cbbc31b8902496f4fae0d91b90b48b646e1b
Jeanine Rioux <jrioux@mvusd.net>
2021-01-11 22:15:17 +0000

    Initial commit

---
+++ main.sql
@@ -0,0 +1,22 @@
+.header on
+.mode column
+
+CREATE TABLE books
+(
+  title TEXT,
+  author TEXT,
+  num_pages INTEGER,
+  rating DOUBLE,
+  publication DATE
+);
+
+INSERT INTO books VALUES
+('Code Girls', 'Liza Mundy', 355, 4.8, 2017),
+('Calculus', 'Ron Larson', 1124, 0.5, 2006),
+('The Holy Bible', 'King James', 1200, 5, 1611 ),
+('1984', 'George Orwell', 328, 5, 1949), 
+('Crime and Punishment' , 'Fyodor Dostoyevsky', 430, 4, 1866);
+
+.print 'Average number of pages'
+SELECT avg(num_pages) AS avg_pages FROM books;
+
